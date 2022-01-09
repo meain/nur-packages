8c69c48d5723a6ad467b59077b29c916fb094543
Abin Simon <mail@meain.io>
2022-01-09 10:40:47 +0530

    Add all packages from meain/nix-channel

--- default.nix
+++ default.nix
@@ -14,7 +14,22 @@
   modules = import ./modules; # NixOS modules
   overlays = import ./overlays; # nixpkgs overlays
 
-  example-package = pkgs.callPackage ./pkgs/example-package { };
-  # some-qt5-package = pkgs.libsForQt5.callPackage ./pkgs/some-qt5-package { };
-  # ...
+  ## personal
+  traffic = pkgs.callPackage ./pkgs/traffic { };
+  gh-issues-to-rss = pkgs.callPackage ./pkgs/gh-issues-to-rss { };
+  tojson = pkgs.callPackage ./pkgs/tojson { };
+  toffee = pkgs.callPackage ./pkgs/toffee { };
+  quickserve = pkgs.callPackage ./pkgs/quickserve { };
+
+  ## external
+  fluent-theme = pkgs.callPackage ./pkgs/fluent-theme { };
+  kmonad = pkgs.callPackage ./pkgs/kmonad { };
+  # notmuch-git = pkgs.callPackage ./pkgs/notmuch-git {};
+  gnomeExtensions.steal-my-focus = pkgs.callPackage ./pkgs/steal-my-focus { };
+  grpc-gateway = pkgs.callPackage ./pkgs/grpc-gateway { };
+  # golang-migrate::tags: postgres mysql redshift cassandra spanner cockroachdb clickhouse file go_bindata github aws_s3 google_cloud_storage godoc_vfs gitlab
+  golang-migrate-pg = pkgs.callPackage ./pkgs/golang-migrate { tags = [ "postgres" "file" ]; }; # with proper build tags
+
+  ## programming
+  # buf = pkgs.callPackage ./pkgs/buf {};
 }
