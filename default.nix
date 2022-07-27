ebe9953e975d850859ee7b50059470882634e018
Abin Simon <mail@meain.io>
2022-07-27 22:04:25 +0530

    aw-watcher-mpd: init at b1a1cf2c

--- default.nix
+++ default.nix
@@ -40,6 +40,7 @@
   spaceman-diff = pkgs.callPackage ./pkgs/spaceman-diff { };
   dbui = pkgs.callPackage ./pkgs/dbui { };
   pulseaudio-virtualmic = pkgs.callPackage ./pkgs/pulseaudio-virtualmic { };
+  aw-watcher-mpd = pkgs.callPackage ./pkgs/aw-watcher-mpd { };
 
   ## programming
   # buf = pkgs.callPackage ./pkgs/buf {};
